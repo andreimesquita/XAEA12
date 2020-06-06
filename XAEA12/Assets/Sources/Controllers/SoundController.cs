4eeb5f2162c264e2b16d9a7ca7c53e80743393e6
Henrique <henrique.dsc@gmail.com>
2020-06-06 18:18:57 -0300

    Added sound controller.

---
+++ XAEA12/Assets/Sources/Controllers/SoundController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class SoundController : MonoBehaviour
+{
+    [SerializeField] private AudioSource _bgMusicSource = null;
+    [SerializeField] private AudioSource _sfxSource = null;
+
+    public static SoundController Instance
+    {
+        get => _instance;
+        set => _instance = value;
+    }
+
+    private static SoundController _instance = null;
+
+    public void PlayMusic(AudioClip clip)
+    {
+        _bgMusicSource.Stop();
+        _bgMusicSource.clip = clip;
+        _bgMusicSource.Play();
+    }
+}
