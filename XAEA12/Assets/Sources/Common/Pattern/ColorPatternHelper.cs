f40c75f1fbfd0864d2a4a0a373dc998fb1a82941
andreimesquita <andreirs@outlook.com>
2020-06-06 20:17:44 -0300

    Pushing ColorPatternHelper + Unit Tests

---
+++ XAEA12/Assets/Sources/Common/Pattern/ColorPatternHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace Sources.Common.Pattern
+{
+    public static class ColorPatternHelper
+    {
+        public static int ToPattern(int[] colorPatterns)
+        {
+            int pattern = 0;
+            for (int i = 0; i < colorPatterns.Length; i++)
+            {
+                int color = colorPatterns[i];
+                pattern |= color << (4 * i);
+            }
+            return pattern;
+        }
+    }
+}
