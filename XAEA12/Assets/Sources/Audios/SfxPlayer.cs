d532e5a7447e474d77b8a7391eb3f5388de407c6
Henrique <henrique.dsc@gmail.com>
2020-06-06 20:57:01 -0300

    Added sfx.

---
+++ XAEA12/Assets/Sources/Audios/SfxPlayer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+[RequireComponent(typeof(AudioSource))]
+public class SfxPlayer : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] _clips = null;
+    [SerializeField] private AudioSource _source = null;
+    [SerializeField] private bool _randomizePitch = false;
+    [SerializeField] private float _pitchRange = 0.1f;
+    
+    private void Awake()
+    {
+        if (_source == null)
+            _source = GetComponent<AudioSource>();
+    }
+
+    public void Play()
+    {
+        int index = Random.Range(0, _clips.Length);
+
+        float pitch = 1.0f;
+        if (_randomizePitch)
+            pitch = Random.Range(1.0f - _pitchRange, 1.0f + _pitchRange);
+
+        AudioClip clip = _clips[index];
+        _source.pitch = pitch;
+        _source.PlayOneShot(clip);
+    }
+}
