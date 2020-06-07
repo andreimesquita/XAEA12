dbb80de3403cbbe75aa9281c8be612ef1149b23c
Henrique <henrique.dsc@gmail.com>
2020-06-07 14:06:36 -0300

    Added sound to explosion.

---
+++ XAEA12/Assets/Sources/SceneObstacles/ExplosionEffect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ExplosionEffect : MonoBehaviour
+{
+    [SerializeField] private SfxPlayer _sfxPlayer = null;
+    
+    void Awake()
+    {
+        _sfxPlayer.Play();
+        StartCoroutine(WaitAndDestroy());
+    }
+
+    private IEnumerator WaitAndDestroy()
+    {
+        yield return new WaitForSeconds(3.0f);
+        Destroy(gameObject);
+    }
+
+}
